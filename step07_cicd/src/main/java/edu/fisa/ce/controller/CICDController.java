9bebc8feae42492fc17b648e39f82df49bca1b6e
nanahj <dlguswjd221@naver.com>
2025-03-24 10:35:27 +0900

    task1. project push

---
+++ step07_cicd/src/main/java/edu/fisa/ce/controller/CICDController.java
@@ -0,0 +1,16 @@
+package edu.fisa.ce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CICDController {
+	
+	// http://localhost:801/fisa1
+	@GetMapping("fisa1")
+	public String reqRes() {
+		System.out.println("reqRes() *******");
+		return "요청 응답 성공";
+	}
+
+}
