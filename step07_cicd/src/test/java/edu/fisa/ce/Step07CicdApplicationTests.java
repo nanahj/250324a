22ad5f7f0b68c7cb40a4e785513383ab9f36bda0
nanahj <dlguswjd221@naver.com>
2025-04-09 10:45:08 +0900

    [UPDATE] test src update

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -7,16 +7,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Step07CicdApplicationTests {
     @Test
     public void testAddition() {
-        int result = 2 + 3;
+        int result = 12 + 3;
         /* 예시 - 서버 시작시에 특정 DATABASE로부터 검색
          * 그 데이터는 꼭 fisa여야만 하는 상황
          * fisa가 없다면 서버 중지 또는 다른 로직 실행
-         *
+         *`
          * 개발 중에 데이터에 대한 test 검증용등으로 사용
          *
          */
         System.out.println("비교 전 ***********");
-        assertEquals(5, result);  // 성공: 5 == 5
+        assertEquals(15, result);  // 성공: 5 == 5
         System.out.println("비교 후 ***********");
     }
 }
